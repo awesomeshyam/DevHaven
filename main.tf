c64f8ba38e50063e3c752751b08040b15713873f
NISHU <awesomeshyam91@gmail.com>
2024-12-02 13:54:09 +0530

    added john-rg

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "dhondhu-rg"
   location = "westeurope"
 }
+
+resource "azurerm_resource_group" "rg-john" {
+  name     = "tondhu-rg"
+  location = "westeurope"
+}
